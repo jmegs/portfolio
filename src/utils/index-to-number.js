c3aff01475666b7af9b2cbf7f15a6ad321916aa1
John Meguerian <johnmeguerian@gmail.com>
2018-02-19 18:37:57 -0500

    Document index-to-number function

--- src/utils/index-to-number.js
+++ src/utils/index-to-number.js
@@ -1,3 +1,7 @@
+// This function takes an array index and returns a zero padded number
+// starting at one rather than zero. Useful for displaying the contents 
+// of an array in a numbered list.
+
 function indexToNumber(idx) {
   return idx < 10 ? `0${idx + 1}` : `${idx + 1}`
 }
