95ef8483d972340d7c6529ee57781316f84d57b9
John Meguerian <johnmeguerian@gmail.com>
2018-02-25 10:52:31 -0500

    Remove unused components

--- src/components/Mono.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { Text } from 'rebass'
-import { textAlign } from 'styled-system'
-
-export default styled(Text)`
-  ${textAlign};
-  font-family: ${props => props.theme.mono};
-`
