c3af12e0a3caa8263e3625a3cc60416e8a4e70dd
John Meguerian <johnmeguerian@gmail.com>
2018-06-01 07:38:21 -0400

    delete previous version files

--- src/components/Image.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { space, maxWidth } from 'styled-system'
-import GatsbyImage from 'gatsby-image'
-
-export default styled(GatsbyImage)`
-  ${space};
-  ${maxWidth};
-`
--- src/utils/index-to-number.js
+++
@@ -1,9 +0,0 @@
-// This function takes an array index and returns a zero padded number
-// starting at one rather than zero. Useful for displaying the contents 
-// of an array in a numbered list.
-
-function indexToNumber(idx) {
-  return idx < 10 ? `0${idx + 1}` : `${idx + 1}`
-}
-
-export default indexToNumber
