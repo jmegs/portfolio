404b6b9973fdea7d7970e2f20f1ad871d8203b2d
John Meguerian <johnmeguerian@gmail.com>
2018-06-03 18:05:04 -0400

    add hover effect

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,116 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { Component } from 'react'
+import styled, { keyframes } from 'styled-components'
 import Link from 'next/link'
 
-const HomePage = () => {
-  return (
-    <Main>
-      <Text>
-        <p>John Meguerian is a product designer and developer in NYC.</p>
-        <p>
-          He’s recently made a <a href="">personal training app</a> for Equinox
-          and an interactive{' '}
-          <Link href="/project/samsung-retail">retail experience</Link> and{' '}
-          <Link href="/project/samsung-dotcom">ecommerce website</Link> for
-          Samsung.
-        </p>
+class HomePage extends Component {
+  state = {
+    swapped: false,
+    overlay: ''
+  }
 
-        <p>
-          He also works on development projects like{' '}
-          <a href="https://github.com/jmegs/static-starter">Static Starter</a>,{' '}
-          <a href="https://codepen.io/collection/XRKYmK/">CSS Posters</a>, and
-          his <a href="https://github.com/jmegs/portfolio">personal website</a>.
-        </p>
+  swap = img => {
+    console.log(`hi ${img}`)
+    this.setState({
+      swapped: true,
+      overlay: img
+    })
+  }
 
-        <p>
-          Find out more about him, check out more work on{' '}
-          <a href="https://dribbble.com/jmegs">dribbble</a> and{' '}
-          <a href="https://github.com/jmegs">github</a>, or say hi on{' '}
-          <a href="https://twitter.com/jmegs">twitter</a> or{' '}
-          <a href="https://instagram.com/jmegs_">instagram</a>.
-        </p>
-      </Text>
-      <PhotoWrap>
-        <Image src="" alt="" className="overlay-photo" />
-        <BaseImage src="/static/john.jpg" alt="Photo of John" />
-      </PhotoWrap>
-    </Main>
-  )
+  reset = () => {
+    this.setState({
+      swapped: false,
+      overlay: ''
+    })
+  }
+
+  render() {
+    const previews = {
+      retail: '/static/retail-preview.jpg',
+      dotcom: '/static/dotcom-preview.jpg',
+      eqx: '/static/eqx-preview.jpg'
+    }
+
+    const eqxLink = (
+      <Link href="#">
+        <a
+          onMouseEnter={() => this.swap(previews.eqx)}
+          onMouseLeave={this.reset}
+          children="personal training app"
+          className="disabled"
+        />
+      </Link>
+    )
+
+    const retailLink = (
+      <Link href="/project/samsung-retail">
+        <a
+          onMouseEnter={() => this.swap(previews.retail)}
+          onMouseLeave={this.reset}
+          children="retail experience"
+        />
+      </Link>
+    )
+
+    const dotcomLink = (
+      <Link href="/project/samsung-dotcom">
+        <a
+          onMouseEnter={() => this.swap(previews.dotcom)}
+          onMouseLeave={this.reset}
+          children="ecommerce website"
+        />
+      </Link>
+    )
+    return (
+      <Main>
+        <Text>
+          <p>John Meguerian is a product designer and developer in NYC.</p>
+          <p>
+            He’s recently made a {eqxLink} for Equinox and an interactive{' '}
+            {retailLink} and {dotcomLink} for Samsung.
+          </p>
+
+          <p>
+            He also works on development projects like{' '}
+            <a href="https://github.com/jmegs/static-starter" className="out">
+              Static Starter
+            </a>,{' '}
+            <a href="https://codepen.io/collection/XRKYmK/" className="out">
+              CSS Posters
+            </a>, and his{' '}
+            <a href="https://github.com/jmegs/portfolio" className="out">
+              personal website
+            </a>.
+          </p>
+
+          <p>
+            Find out more about him, check out more work on{' '}
+            <a href="https://dribbble.com/jmegs" className="out">
+              dribbble
+            </a>{' '}
+            and{' '}
+            <a href="https://github.com/jmegs" className="out">
+              github
+            </a>, or say hi on{' '}
+            <a href="https://twitter.com/jmegs" className="out">
+              twitter
+            </a>{' '}
+            or{' '}
+            <a href="https://instagram.com/jmegs_" className="out">
+              instagram
+            </a>.
+          </p>
+        </Text>
+        <PhotoWrap>
+          <OverlayImage src={this.state.overlay} alt="" />
+          <BaseImage
+            src="/static/john.jpg"
+            alt="Photo of John"
+            fade={this.state.swapped}
+          />
+        </PhotoWrap>
+      </Main>
+    )
+  }
 }
 
 export default HomePage
@@ -61,6 +136,16 @@ const Text = styled.article`
     color: inherit;
   }
 
+  a.disabled {
+    cursor: not-allowed;
+    text-decoration: none;
+    border-bottom: 1px #0d0d0d dotted;
+  }
+
+  a.out {
+    cursor: alias;
+  }
+
   p {
     margin-bottom: 20px;
   }
@@ -77,6 +162,8 @@ const Text = styled.article`
 
 const PhotoWrap = styled.div`
   align-self: center;
+  display: grid;
+  grid-template: 100% / 100%;
 `
 
 const Image = styled.img`
@@ -88,4 +175,29 @@ const Image = styled.img`
 const BaseImage = Image.extend`
   height: 80vh;
   object-position: center top;
+  grid-column: 1 / span 1;
+  grid-row: 1 / span 1;
+  opacity: ${props => (props.fade ? 0.2 : 1)};
+  transition: opacity 250ms ease;
+`
+
+const OverlayImage = Image.extend`
+  grid-column: 1 / span 1;
+  grid-row: 1 / span 1;
+  z-index: 2;
+  align-self: center;
+  justify-self: center;
+  width: 128%;
+  animation: ${fadeInFwd} 4000ms cubic-bezier(0.39, 0.575, 0.565, 1) both;
+`
+
+const fadeInFwd = keyframes`
+ 0% {
+    transform: translateZ(-80px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateZ(0);
+    opacity: 1;
+  }
 `
