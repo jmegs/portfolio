b7925ba0f3224e4a351c46bc59690ef810d89c5c
John Meguerian <johnmeguerian@gmail.com>
2018-11-29 09:01:02 -0500

    page transition framework setup

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+import App, { Container } from 'next/app'
+import React from 'react'
+import { PageTransition } from 'next-page-transitions'
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+        <PageTransition timeout={500} classNames="page-transition">
+          <Component {...pageProps} />
+        </PageTransition>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transition: opacity 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}</style>
+      </Container>
+    )
+  }
+}
