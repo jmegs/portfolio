3c320f00cacd6231d3eae70d6a9e37f84daa0cbc
John Meguerian <johnmeguerian@gmail.com>
2019-01-09 08:15:59 -0500

    add MDX boilerplate code

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 const withSass = require('@zeit/next-sass')
+const withMDX = require('@zeit/next-mdx')()
+module.exports = withSass(
+  withMDX({
+    webpack: config => {
+      // Fixes npm packages that depend on 'fs' module
+      config.node = {
+        fs: 'empty'
+      }
 
-module.exports = withSass({
-  webpack: config => {
-    // Fixes npm packages that depend on 'fs' module
-    config.node = {
-      fs: 'empty'
+      return config
     }
-
-    return config
-  }
-})
+  })
+)
