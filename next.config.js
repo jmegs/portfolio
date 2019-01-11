077d982cd6b84524fcc48058140ba2ff95c71547
John Meguerian <johnmeguerian@gmail.com>
2019-01-10 19:03:42 -0500

    Revert styled components decision
    Over engineering, my friend. This reverts commit c76800e200f05318cd521a3bae04e4f6ddc0c33d.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const withSass = require('@zeit/next-sass')
-const withMDX = require('@zeit/next-mdx')()
-module.exports = withSass(
-  withMDX({
-    webpack: config => {
-      // Fixes npm packages that depend on 'fs' module
-      config.node = {
-        fs: 'empty'
-      }
 
-      return config
+module.exports = withSass({
+  webpack: config => {
+    // Fixes npm packages that depend on 'fs' module
+    config.node = {
+      fs: 'empty'
     }
-  })
-)
+
+    return config
+  }
+})
