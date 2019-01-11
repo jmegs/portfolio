4f75fcbbcb420134231e06951db76dfcc88802b1
John Meguerian <johnmeguerian@gmail.com>
2019-01-10 19:47:12 -0500

    implement new homepage

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
 import Layout from '../components/Layout'
-import Projects from '../components/Projects'
-import Social from '../components/Social'
+import Link from 'next/link'
+
+const Samsung = (
+  <Link href="/project/samsung">
+    <a>Samsung</a>
+  </Link>
+)
+const Equinox = (
+  <Link href="/project/equinox">
+    <a>Equinox</a>
+  </Link>
+)
+const Squarespace = <a href="https://squarespace.com">Squarespace</a>
+const Instagram = <a href="https://instagram.com/jmegs_">Instagram</a>
+const Twitter = <a href="https://twitter.com/jmegs">Instagram</a>
+const Github = <a href="https://github.com/jmegs">Github</a>
+const Linkedin = <a href="https://linkedin.com/in/johnmeguerian/">Linkedin</a>
 
 const HomePage = () => {
   return (
-    <Layout dark>
-      <h1 className="big-serif home-title">
-        John Meguerian is a product designer and occasional developer in NYC.
-        Currently at Squarespace.
+    <Layout>
+      <h1>
+        John Meguerian is a product designer and occasional developer at{' '}
+        {Squarespace}. View his recent work for {Equinox} and {Samsung} or visit
+        on {Instagram}, {Twitter}, {Github}, or {Linkedin}.
       </h1>
-      <Projects />
-      <Social />
     </Layout>
   )
 }
