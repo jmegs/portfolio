0a3a1be3ad1c92e9d637c4353f25c9466f06dded
John Meguerian <johnmeguerian@gmail.com>
2019-03-07 11:14:19 -0500

    add example image shortcode

---
+++ src/components/image.js
@@ -0,0 +1,14 @@
+// Generate an image tag with responsive images served from cloudinary
+
+// @TODO change to `<img>`, add cloudinary urls, figure out image logic
+
+// @TODO in config add
+// config.addShortcode("image", require("./src/components/image.js"))
+
+// module.exports = (id, alt = 'Missing alt text') => {
+//   return `<picture>
+//   <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
+//   <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
+//   <img src="/images/${url}?nf_resize=fit&w=500" alt="${alt}" />
+// </picture>`
+// }
