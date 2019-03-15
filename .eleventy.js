765c28900617ea5c35c7ecac3c87f7c136045a4c
John Meguerian <johnmeguerian@gmail.com>
2019-03-15 08:11:55 -0400

    add markdown block images

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function(config) {
   // add markdown plugins
-  // let markdownIt = require('markdown-it')
-  // let blockImagePlugin = require('markdown-it-block-image')
-  // let options = { html: true }
-  // let markdownLib = markdownIt(options).use(blockImagePlugin)
-  // eleventyConfig.setLibrary('md', markdownLib)
+  let markdownIt = require('markdown-it')
+  let blockImagePlugin = require('markdown-it-block-image')
+  let options = { html: true }
+  let markdownLib = markdownIt(options).use(blockImagePlugin)
+  config.setLibrary('md', markdownLib)
 
   // Layout aliases can make templates more portable
   config.addLayoutAlias('default', 'layouts/base.html')
