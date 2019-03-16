6dc95424f84dd019b9df482eddde103cac7b272b
John Meguerian <johnmeguerian@gmail.com>
2019-03-16 14:51:23 -0400

    actually toggle the dark mode class

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const darkModeToggle = document.querySelector('.nav-darkmode-toggle')
 
 darkModeToggle.addEventListener('click', () => {
-  console.log('dark mode!')
+  document.body.classList.toggle('dark-mode')
 })
