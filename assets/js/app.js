c7ad69440e45c5677a0b63052043489b18567b27
John Meguerian <johnmeguerian@gmail.com>
2019-03-17 23:01:42 -0400

    wire up scroll to top link

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,18 @@
+/* 
+  Init lazy loading images 
+  everything with class .lazyload will do so
+*/
+import 'lazysizes'
+
+/* Dark Mode 👇 */
 const darkModeToggle = document.querySelector('.nav-darkmode-toggle')
 
 darkModeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode')
 })
 
-import 'lazysizes'
+/* Elevator Scroll 👇 */
+const scrollToTop = document.querySelector('.js-scrollToTop')
+scrollToTop.addEventListener('click', () => {
+  window.scroll({ top: 0 })
+})
