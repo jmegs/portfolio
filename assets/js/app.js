57984ae68a9b60f8d7c3209cff594ee5d5537a3a
John Meguerian <johnmeguerian@gmail.com>
2019-03-20 09:15:15 -0400

    add dark mode persistence

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,39 @@
   everything with class .lazyload will do so
 */
 import 'lazysizes'
+import { getItemFromStorage, storeItem } from './localstorage'
 
 /* Dark Mode 👇 */
 const darkModeToggle = document.querySelector('.nav-darkmode-toggle')
 
+function setDarkMode(status) {
+  storeItem('darkMode', status)
+  console.log(`darkMode: ${status}`)
+}
+
+function isDarkModeEnabled() {
+  if (getItemFromStorage('darkMode')) {
+    return true
+  } else {
+    return false
+  }
+}
+
 darkModeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode')
+  if (isDarkModeEnabled()) {
+    document.body.classList.remove('dark-mode')
+    setDarkMode(false)
+  } else {
+    document.body.classList.add('dark-mode')
+    setDarkMode(true)
+  }
 })
+
+window.onload = () => {
+  if (isDarkModeEnabled()) {
+    document.body.classList.add('dark-mode')
+  }
+}
 
 /* Elevator Scroll 👇 */
 const scrollToTop = document.querySelector('.js-scrollToTop')
---
+++ assets/js/localstorage.js
@@ -0,0 +1,29 @@
+export const getItemFromStorage = key => {
+  if (!localStorage) return
+
+  try {
+    return JSON.parse(localStorage.getItem(key))
+  } catch (err) {
+    return null
+  }
+}
+
+export const storeItem = (key, value) => {
+  if (!localStorage) return
+
+  try {
+    return localStorage.setItem(key, JSON.stringify(value))
+  } catch (err) {
+    return null
+  }
+}
+
+export const removeItemFromStorage = key => {
+  if (!localStorage) return
+
+  try {
+    return localStorage.removeItem(key)
+  } catch (err) {
+    return null
+  }
+}
