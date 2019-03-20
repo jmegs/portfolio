56db88908b13b705cfca4fba732b17ed4b216820
John Meguerian <johnmeguerian@gmail.com>
2019-03-20 19:19:52 -0400

    remove dark mode toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,39 +3,6 @@
   everything with class .lazyload will do so
 */
 import 'lazysizes'
-import { getItemFromStorage, storeItem } from './localstorage'
-
-/* Dark Mode 👇 */
-const darkModeToggle = document.querySelector('.nav-darkmode-toggle')
-
-function setDarkMode(status) {
-  storeItem('darkMode', status)
-  console.log(`darkMode: ${status}`)
-}
-
-function isDarkModeEnabled() {
-  if (getItemFromStorage('darkMode')) {
-    return true
-  } else {
-    return false
-  }
-}
-
-darkModeToggle.addEventListener('click', () => {
-  if (isDarkModeEnabled()) {
-    document.body.classList.remove('dark-mode')
-    setDarkMode(false)
-  } else {
-    document.body.classList.add('dark-mode')
-    setDarkMode(true)
-  }
-})
-
-window.onload = () => {
-  if (isDarkModeEnabled()) {
-    document.body.classList.add('dark-mode')
-  }
-}
 
 /* Elevator Scroll 👇 */
 const scrollToTop = document.querySelector('.js-scrollToTop')
