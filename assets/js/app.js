bb47068a18feab2ac17e711e7f64839ad2d77784
John Meguerian <johnmeguerian@gmail.com>
2019-03-25 09:06:49 -0400

    add swup for page transitions

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,9 @@
   everything with class .lazyload will do so
 */
 import 'lazysizes'
+import Swup from 'swup'
+
+const swup = new Swup()
 
 /* Elevator Scroll 👇 */
 const scrollToTop = document.querySelector('.js-scrollToTop')
