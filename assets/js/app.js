8b475ca4d2ff6072448464919012d43d7ba911eb
John Meguerian <johnmeguerian@gmail.com>
2019-03-27 22:07:57 -0400

    remove elevator js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,3 @@
   everything with class .lazyload will do so
 */
 import 'lazysizes'
-
-/* Elevator Scroll 👇 */
-const scrollToTop = document.querySelector('.js-scrollToTop')
-scrollToTop.addEventListener('click', () => {
-  window.scroll({ top: 0 })
-})
