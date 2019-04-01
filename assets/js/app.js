972be56ea2c7e7c35deb8b465d778c7d64b2a8dd
John Meguerian <johnmeguerian@gmail.com>
2019-04-01 07:54:29 -0400

    Add vh-check library
    fixes title not displaying in view on mobile safari
    
    for certain browsers (looking at you mobile safari) vh doesnt
    consider the browser chrome in its calculations.
    
    this change enables a css variable called --vh-offset that is
    set when needed. the min-height of the body is now set to
    calc(100vh - var(--vh-offset))

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,12 @@
   everything with class .lazyload will do so
 */
 import 'lazysizes'
+
+// sets css custom property "--vh-offset" if needed to compensate for the mobile
+// browser bar and chrome, else returns 0. used like so:
+// height: calc(100vh - var(--vh-offset));
+// https://github.com/Hiswe/vh-check
+
+import vhCheck from 'vh-check'
+const check = vhCheck()
+console.log(check)
