d4b31cc29605a229924f7d093222296e6b99368c
John Meguerian <johnmeguerian@gmail.com>
2019-04-02 10:45:30 -0400

    Add Animations and Transitions
    * create new file _animations.less to house animation logic
    * main container animates in on page load and lazyloaded images
      fade in  when they load.
    * the lazyload animation required adding a negative data-expand
      attribute to the image component's figure tag

--- src/_includes/components/image.js
+++ src/_includes/components/image.js
@@ -6,7 +6,7 @@
 const baseUrl = 'https://res.cloudinary.com/jmegs'
 
 module.exports = (id, alt = 'Missing alt text') => {
-  return `<figure><img class="lazyload" alt="${alt}"
+  return `<figure data-expand="-20"><img class="lazyload" alt="${alt}"
   data-src="${baseUrl}/c_scale,w_1366,f_auto/${id}.jpg"
   data-srcset="${baseUrl}/c_scale,w_640,f_auto/${id}.jpg 640w, 
   ${baseUrl}/c_scale,w_1366,f_auto/${id}.jpg 1366w, 
