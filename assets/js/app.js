16b745a1aa4624600c719d15ebc13725eecef454
John Meguerian <johnmeguerian@gmail.com>
2019-04-09 10:23:14 -0400

    Randomly Set the Site's Hue
    when the page loads, the `--hue` custom property is randomly set to a
    number between 0 and 360. The background and text colors inherit this
    value.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-console.log('hi')
+let root = document.documentElement
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function setRandomHue() {
+  let randomHue = getRandomInt(360)
+  root.style.setProperty("--hue", randomHue)
+}
+
+setRandomHue()
