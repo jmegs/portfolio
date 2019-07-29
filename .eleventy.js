a1c9306ec86e0a3fc52465ffcce1e6be4c4ddb44
John Meguerian <johnmeguerian@gmail.com>
2019-07-29 16:42:37 -0400

    add netlifycms boilerplate

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
-module.exports = {
-  dir: {
-    input: 'source',
-    output: 'dist',
-    layouts: 'layouts',
-    includes: 'includes',
-    data: 'data'
+module.exports = function(config) {
+  // pass through cms files
+  config.addPassthroughCopy('admin')
+
+  return {
+    dir: {
+      input: 'source',
+      output: 'dist',
+      layouts: 'layouts',
+      includes: 'includes',
+      data: 'data'
+    }
   }
 }
