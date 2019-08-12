aefd0471d0bef051e3698b3a992cede6b054de23
John Meguerian <johnmeguerian@gmail.com>
2019-08-12 18:42:26 -0400

    remove webpack config

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  entry: './source/assets/javascript/main.js',
-  output: {
-    filename: 'assets/javascript/[name].js',
-    path: path.resolve(__dirname, '.tmp/dist')
-  }
-}
