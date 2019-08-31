868a928ac60cbbee63b2c9d43eef993ba9cb1b1c
John Meguerian <johnmeguerian@gmail.com>
2019-08-31 11:12:40 -0400

    fix footer padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   const { social } = useSiteMeta()
 
   return (
-    <footer className="social">
+    <footer className="u-box social">
       {social.map(service => (
         <a className="social__link" href={service.url} key={service.name}>
           {service.name}
